3990c9a8393c56f378c7a33d15ab427cd356efdf
Caleb Doxsey <caleb@doxsey.net>
2017-07-18 12:45:54 -0400

    Update consul-server.bash

--- machines/consul-server.bash
+++ machines/consul-server.bash
@@ -3,7 +3,6 @@
 # In User Data do:
 
 # #!/bin/bash
-# export ATLAS_USERNAME=...
-# export ATLAS_TOKEN=...
-# export CONSUL_VERSION=0.7.5
-# curl https://raw.githubusercontent.com/calebdoxsey/cloud-machine/master/consul-server.bash | sudo -E /bin/bash
+# export CONSUL_VERSION=0.8.5
+# curl https://raw.githubusercontent.com/badgerodon/infrastructure/master/machines/consul-server.bash | sudo -E /bin/bash
+
