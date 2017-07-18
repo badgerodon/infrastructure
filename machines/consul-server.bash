9915b4a5282698d414f2bd9709d13e632569fe34
Caleb Doxsey <caleb@doxsey.net>
2017-07-18 12:45:08 -0400

    Update and rename .gitignore to machines/consul-server.bash

---
+++ machines/consul-server.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# In User Data do:
+
+# #!/bin/bash
+# export ATLAS_USERNAME=...
+# export ATLAS_TOKEN=...
+# export CONSUL_VERSION=0.7.5
+# curl https://raw.githubusercontent.com/calebdoxsey/cloud-machine/master/consul-server.bash | sudo -E /bin/bash
